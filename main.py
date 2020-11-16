c829c954292996face1ee95fa1d466a7d28131f3
ggornes <gerardogornes@gmail.com>
2020-11-16 13:55:46 +0800

    show warning if ilegal move

--- main.py
+++ main.py
@@ -1,54 +1,66 @@
 from board import Board
 from save_game import create_game_file, save_turn
+import utils
+import os
 
 
 def start_game():
+    turn_counter = 0
     print("Starting new chess game")
     board = Board()
-    b = board.board_to_list_of_list()
+    initial_board = board.board_to_list_of_list()
     game_input = ''
-    create_game_file(board, b)
+    create_game_file(board, initial_board)
 
     # game loop
     while game_input != "quit":
+        os.system('cls' if os.name == 'nt' else 'clear')
         board.print_board()
         # board.board_to_str()
-        print("Board representation as a list: ")
-        print(board.board_to_list())
-        print("Board representation as a list of list: ")
+        # print("Board representation as a list: ")
+        # print(board.board_to_list())
+
+
+        # print("Board representation as a list of list: ")
         b = board.board_to_list_of_list()
-        print(b)
-        print("Board representation in one single line: ")
-        print(board.board_to_str())
-        print("Board representation in one single line 2: ")
-        print(board.list_of_list_board_to_str(b))
+        #print(b)
 
+
+        # print("Board representation in one single line: ")
+        # print(board.board_to_str())
+        # print("Board representation in one single line: ")
+        # print(utils.list_of_list_board_to_str(b))
+
+        if turn_counter % 2 == 0:
+            print("White turn")
+        else:
+            print("Black turn")
         move = input("Move:")
-        print("file, rank, x, y")
-        # file, rank, x, y = board.square_to_list_index_position(move)
-        # print(file + ', ' + rank + ', ' + str(x) + ', ' + str(y))
+
         square_from, square_to = board.parse_move_to_square(move)
-        print(square_from, square_to)
         file_from, rank_from, x_from, y_from = board.parse_square_to_list_index_position(str(square_from))
-        print(file_from + ', ' + rank_from + ', ' + str(x_from) + ', ' + str(y_from))
         file_to, rank_to, x_to, y_to = board.parse_square_to_list_index_position(str(square_to))
-        print(file_to + ', ' + rank_to + ', ' + str(x_to) + ', ' + str(y_to))
+        print("Moving " + square_from + " (" + str(x_from) + ", " + str(y_from) + ")" + " to " + square_to + " (" + str(
+            x_to) + ", " + str(y_to) + ")")
 
-        print("piece at board string")
-        board_as_string = board.list_of_list_board_to_str(b)
-        print(board_as_string[8*x_from + y_from])
-        print(board_as_string[8 * x_to + y_to])
-        print("piece index")
-        index_from = 8*x_from + y_from + 1
-        print(index_from, "index_from: ")
-        index_to = 8*x_to + y_to + 1
-        print(index_to, "index_to: ")
-
+        # Convert the matrix board to string
+        board_as_string = utils.list_of_list_board_to_str(b)
+        index_from = 8 * x_from + y_from + 1
+        index_to = 8 * x_to + y_to + 1
+        print("Moving " + board_as_string[8 * x_from + y_from] + " str_position: " + str(index_from - 1) + " to " +
+              board_as_string[8 * x_to + y_to] + " str_position: " + str(index_to - 1))
 
         game_input = move
-        board.move(move)
-
-        save_turn(index_from, index_to)
+        # ToDo: validate move
+        if board.validate_move(move):
+            board.move(move)
+            # The moving piece
+            moving_piece = board_as_string[8 * x_from + y_from]
+            save_turn(moving_piece, index_from, index_to)
+            turn_counter += 1
+        else:
+            print("----- WARNING: Illegal move, please try again: ------")
+            input("Press Enter to continue...")
 
 
 start_game()
