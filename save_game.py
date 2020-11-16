91717dbeaf562d66490687e42ee827993a547b13
ggornes <gerardogornes@gmail.com>
2020-11-16 11:48:31 +0800

    added method to save the game after a turn is played using seek()

--- save_game.py
+++ save_game.py
@@ -1,11 +1,23 @@
 import board as board
 import os
 
-def create_game_file(board):
+def create_game_file(board, b):
     if not os.path.exists('saved_games'):
         os.makedirs('saved_games')
 
     with open(file='saved_games/new_game.txt', mode='w', encoding='utf-8') as file:
-        file.writelines([board.board_to_str()])
+        file.writelines([board.list_of_list_board_to_str(b)])
 
-# def save_turn(board):
+def save_turn(p_from, p_to):
+    with open(file='saved_games/new_game.txt', mode='r+', encoding='utf-8') as file:
+        initial_position = file.tell()
+        print("Initial position: ", initial_position)
+
+        from_value = file.seek(p_from - 1, initial_position)
+        print("file seek p_from: ", from_value)
+        file.write("#")
+
+        to_value = file.seek(p_to - 1, initial_position)
+        print("file seek p_to: ", to_value)
+        file.write("@")
+
