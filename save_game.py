d10497c449b3a09616e723ea809889facda09017
ggornes <gerardogornes@gmail.com>
2020-11-16 13:54:17 +0800

    game state is saved after each turn

--- save_game.py
+++ save_game.py
@@ -1,23 +1,22 @@
 import board as board
 import os
+import utils
 
 def create_game_file(board, b):
     if not os.path.exists('saved_games'):
         os.makedirs('saved_games')
 
     with open(file='saved_games/new_game.txt', mode='w', encoding='utf-8') as file:
-        file.writelines([board.list_of_list_board_to_str(b)])
+        file.writelines([utils.list_of_list_board_to_str(b)])
 
-def save_turn(p_from, p_to):
+def save_turn(moving_piece, position_from, position_to):
     with open(file='saved_games/new_game.txt', mode='r+', encoding='utf-8') as file:
         initial_position = file.tell()
-        print("Initial position: ", initial_position)
-
-        from_value = file.seek(p_from - 1, initial_position)
-        print("file seek p_from: ", from_value)
-        file.write("#")
-
-        to_value = file.seek(p_to - 1, initial_position)
-        print("file seek p_to: ", to_value)
-        file.write("@")
+        file.seek(position_from - 1, initial_position)
+        #print("file seek p_from: ", from_value)
+        # when moving a piece, it always leaves a blank space behind
+        file.write(".")
+        file.seek(position_to - 1, initial_position)
+        # print("file seek p_to: ", to_value)
+        file.write(moving_piece)
 
