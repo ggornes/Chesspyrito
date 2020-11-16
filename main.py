a2e2750a68167ae0885ff2fdf7d3ceda74107eed
ggornes <gerardogornes@gmail.com>
2020-11-16 14:12:55 +0800

    added wrong input warning and quit game

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 def start_game():
     turn_counter = 0
     print("Starting new chess game")
+    # Initialize the board and save it as a list of list
     board = Board()
     initial_board = board.board_to_list_of_list()
     game_input = ''
@@ -14,53 +15,49 @@ def start_game():
 
     # game loop
     while game_input != "quit":
+        # clear screen and print board
         os.system('cls' if os.name == 'nt' else 'clear')
         board.print_board()
-        # board.board_to_str()
-        # print("Board representation as a list: ")
-        # print(board.board_to_list())
-
-
-        # print("Board representation as a list of list: ")
+        # save the board as a list of list
         b = board.board_to_list_of_list()
-        #print(b)
-
-
-        # print("Board representation in one single line: ")
-        # print(board.board_to_str())
-        # print("Board representation in one single line: ")
-        # print(utils.list_of_list_board_to_str(b))
-
+        # keep track of which player turn is
         if turn_counter % 2 == 0:
             print("White turn")
         else:
             print("Black turn")
-        move = input("Move:")
+        move = input('Enter Move ("quit" to exit game): ')
+        try:
+            square_from, square_to = board.parse_move_to_square(move)
+            file_from, rank_from, x_from, y_from = board.parse_square_to_list_index_position(str(square_from))
+            file_to, rank_to, x_to, y_to = board.parse_square_to_list_index_position(str(square_to))
+            # print("Moving " + square_from + " (" + str(x_from) + ", " + str(y_from) + ")" + " to " + square_to + " (" + str(
+            #     x_to) + ", " + str(y_to) + ")")
 
-        square_from, square_to = board.parse_move_to_square(move)
-        file_from, rank_from, x_from, y_from = board.parse_square_to_list_index_position(str(square_from))
-        file_to, rank_to, x_to, y_to = board.parse_square_to_list_index_position(str(square_to))
-        print("Moving " + square_from + " (" + str(x_from) + ", " + str(y_from) + ")" + " to " + square_to + " (" + str(
-            x_to) + ", " + str(y_to) + ")")
+            # Convert the matrix board to string
+            board_as_string = utils.list_of_list_board_to_str(b)
+            index_from = 8 * x_from + y_from + 1
+            index_to = 8 * x_to + y_to + 1
+            # print("Moving " + board_as_string[8 * x_from + y_from] + " str_position: " + str(index_from - 1) + " to " +
+            #       board_as_string[8 * x_to + y_to] + " str_position: " + str(index_to - 1))
 
-        # Convert the matrix board to string
-        board_as_string = utils.list_of_list_board_to_str(b)
-        index_from = 8 * x_from + y_from + 1
-        index_to = 8 * x_to + y_to + 1
-        print("Moving " + board_as_string[8 * x_from + y_from] + " str_position: " + str(index_from - 1) + " to " +
-              board_as_string[8 * x_to + y_to] + " str_position: " + str(index_to - 1))
+            game_input = move
 
-        game_input = move
-        # ToDo: validate move
-        if board.validate_move(move):
-            board.move(move)
-            # The moving piece
-            moving_piece = board_as_string[8 * x_from + y_from]
-            save_turn(moving_piece, index_from, index_to)
-            turn_counter += 1
-        else:
-            print("----- WARNING: Illegal move, please try again: ------")
+            if board.validate_move(move):
+                board.move(move)
+                # The moving piece
+                moving_piece = board_as_string[8 * x_from + y_from]
+                save_turn(moving_piece, index_from, index_to)
+                turn_counter += 1
+            else:
+                print("----- WARNING: Illegal move, please try again: ------")
+                input("Press Enter to continue...")
+        except:
+            if move == 'quit':
+                print("Exiting game")
+                break
+            print("Input is not a move")
             input("Press Enter to continue...")
+            pass
 
 
 start_game()
